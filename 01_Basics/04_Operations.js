4095ba7bc351667df7e7777924f2fc7876c06a9a
Keshav202 <130147146+Keshav202@users.noreply.github.com>
2025-01-29 22:33:45 +0000

    Some Operations in js

---
+++ 01_Basics/04_Operations.js
@@ -0,0 +1,29 @@
+// How to find negative value
+
+let value = 3
+let negvalue = -3
+console.log(negvalue)
+
+// Addition, Substraction, Multiplication, Division, Modulor, Power
+
+console.table([2+2, 2-2, 2*2, 2/2, 2%2, 2**2])
+
+// Adding Strings
+
+let str1 = "Hello"
+let str2 = " Keshav"
+let str3 = str1 + str2
+
+console.log(str3)
+
+// 
+
+console.log( 1 + "2")
+console.log("1" + 2)
+console.log(1 + 1 + "2")
+console.log("1" + 2 + 3)
+
+// Boolean statement: print true and false
+
+console.log(+true);
+console.log(+"");
