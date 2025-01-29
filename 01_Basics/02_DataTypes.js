bb3d0bd5b410151669a5c6b2cd547ba1d8c61446
Keshav202 <130147146+Keshav202@users.noreply.github.com>
2025-01-29 21:32:44 +0000

    data types in js

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,8 @@
+const uid = 22010
+let name = "Keshav"
+let age = 20
+let city = "madhubani"
+let state;
+
+console.table([uid, name, age, city, state])
+console.table([typeof uid, typeof name, typeof age, typeof city, typeof state])
