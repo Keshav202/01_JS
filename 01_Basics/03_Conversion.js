f57592548b5c1b865d66fd2e385e138c356a3f79
Keshav202 <130147146+Keshav202@users.noreply.github.com>
2025-01-29 22:12:32 +0000

    DataType Conversion

---
+++ 01_Basics/03_Conversion.js
@@ -0,0 +1,38 @@
+// String to NUmber conversion
+
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// Boolean to Number Conversion
+
+let bool = true
+
+console.log(typeof bool);
+
+let boolToNum = Number(bool)
+console.log(typeof boolToNum);
+console.log(boolToNum);
+
+// NUmber to Boolean
+
+let number = 1 // 1 => true; 0 => false; "empty" => false;  "String" => true
+console.log(typeof number);
+
+let numToBool = Boolean(number)
+console.log(typeof numToBool)
+console.log(numToBool)
+
+// Number to string
+
+let num = 56
+console.log(typeof num)
+
+let numToString = String(num)
+console.log(typeof numToString)
+console.log(numToString)
