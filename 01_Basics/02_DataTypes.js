6a2ad0706ac4bc46cb3f430cc06daaa8c4f0929a
Keshav202 <130147146+Keshav202@users.noreply.github.com>
2025-01-30 10:59:01 +0000

    compare

--- 01_Basics/02_DataTypes.js
+++ 01_Basics/02_DataTypes.js
@@ -1,8 +1,39 @@
-const uid = 22010
-let name = "Keshav"
-let age = 20
-let city = "madhubani"
-let state;
+/* Two Types of Data Types
+1. Primitiv => 7 types => String, Number, Boolean, Null, Undefiened, Symbol, BigInt
+2. Non-Primitive/Referance => Array, Objects, Function
+*/
 
-console.table([uid, name, age, city, state])
-console.table([typeof uid, typeof name, typeof age, typeof city, typeof state])
+// Primitive
+
+// Number Data Type
+const uid = 22010
+// String Data Type
+let name = "Keshav"
+// Boolean
+let logIn = true
+// Null
+const temp = ""
+// Undefined
+let state;
+// Symbol
+const ID = Symbol('123')
+const ID2 = Symbol('123')
+// BigInt
+const bigNum = 234455675535364n
+
+
+console.table([uid, name, logIn, temp, state, ID, ID2, bigNum])
+console.table([typeof uid, typeof name, typeof logIn, typeof temp, typeof state, typeof ID, typeof ID2, typeof bigNum])
+console.log(ID === ID2)
+
+// Non-Primitive
+
+const arrHero = ["ironman", "spiderman", "captainAmerica"];
+let myObj = {
+    name : " keshav",
+    age : 20,
+    state : "Bihar"
+}
+
+console.log(arrHero)
+console.log(myObj)
